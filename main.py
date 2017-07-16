6dca7255bffc87da3603e0b5bf873db7b2eb805e
Nicole Freed <nicole.freed@gmail.com>
2017-07-15 18:59:14 -0700

    fill out login() and register() functions

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, session
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -29,13 +29,47 @@ class User(db.Model):
         self.password = password
 
 
-@app.route('/login')
+@app.route('/login', methods=['POST', 'GET'])
 def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user = User.query.filter_by(email=email).first()
+        if user and user.password == password:
+            # TODO = "remember" that user has logged in; session is a dictionary
+            return redirect('/')
+        else:
+            # TODO = explain why login failed
+            return "<h1>Error!</h1>"
+
     return render_template('login.html')
 
-@app.route('/register')
+@app.route('/register', methods=['POST', 'GET'])
 def register():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        verify = request.form['verify']
+
+        # TODO = validate user's data
+
+        existing_user = User.query.filter_by(email=email).first()
+        if not existing_user:
+            new_user = User(email, password)
+            db.session.add(new_user)
+            db.session.commit()
+            # TODO = remember that the user has logged in from one request to the next
+            return redirect('/')
+        else:
+            # TODO = better response messaging
+            return "<h1>Duplicate User</h1>"
+
     return render_template('register.html')
+
+@app.route('/logout')
+def logout():
+    del session['email']
+    return redirect('/')
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
